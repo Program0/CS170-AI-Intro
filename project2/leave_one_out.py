c9b73a950882f1f4e242575d6763ec372d9f7ff1
Marlo Zeroth <Program0@users.noreply.github.com>
2018-12-17 12:47:05 -0800

    Version 4
    Added my own search algorithm. Does not always provide search results.

--- project2/leave_one_out.py
+++ project2/leave_one_out.py
@@ -9,6 +9,7 @@ def leave_one_out_cross_validation(data, current_set, feature_to_add):
     actual_class_label = 0
     classified_label = 0
     set_of_features = set(list(current_set))
+    accuracy = 0
     if feature_to_add <= number_of_features and feature_to_add > 0:
         set_of_features.add(feature_to_add)
     list_of_features = list(set_of_features)
@@ -20,6 +21,38 @@ def leave_one_out_cross_validation(data, current_set, feature_to_add):
         classified_label = nearest_neighbor(data, list_of_features, row)
         if(class_label == classified_label):
             number_correct += 1
-    # Get the accuracy for the nearest neighbor classifier    
-    accuracy = float(number_correct/rows)
+    # Get the accuracy for the nearest neighbor classifier     
+    accuracy = number_correct/rows
+
+    return accuracy
+
+def leave_one_out_modified(data, current_set, feature_to_add, threshold):
+    rows = data.shape[0]
+    number_of_features = data.shape[1] - 1
+    number_correct = 0
+    number_wrong = 0
+    actual_class_label = 0
+    classified_label = 0
+    set_of_features = set(list(current_set))
+    accuracy = 0
+    if feature_to_add <= number_of_features and feature_to_add > 0:
+        set_of_features.add(feature_to_add)
+    list_of_features = list(set_of_features)
+    # For each point in data, go through each data point and 
+    # calculate it's nearest neighbor. Count how many classifications
+    # are correct. If a threshold of wrong classifications is achieved
+    # return 0.
+    for row in range(0, rows):
+        if (number_wrong > threshold):
+            return 0
+            
+        class_label = data[row][0]
+        classified_label = nearest_neighbor(data, list_of_features, row)
+        if(class_label == classified_label):
+            number_correct += 1
+        else:
+            number_wrong +=1
+    # Get the accuracy for the nearest neighbor classifier     
+    accuracy = number_correct/rows
+
     return accuracy
--- project2/search.py
+++ project2/search.py
@@ -1,6 +1,7 @@
 import numpy as np
+import time
 from random import randint
-from leave_one_out import leave_one_out_cross_validation
+from leave_one_out import leave_one_out_cross_validation, leave_one_out_modified
 
 def print_detail(data):
     number_of_features = data.shape[1]
@@ -10,8 +11,15 @@ def print_detail(data):
     feature_set = list(range(1,number_of_features))
     accuracy = leave_one_out_cross_validation(data, feature_set, 0)
     print('Running nearest neighbor with all %d features, ' %(number_of_features-1), 
-          'using \"leaving-one-out\" evaluation, I get and accuracy of %.2f' %(accuracy*100)
+          'using \"leaving-one-out\" evaluation, I get accuracy of %.2f' %(accuracy*100)
           , '%\n')
+          
+    empty_set = set()
+    accuracy = leave_one_out_cross_validation(data, empty_set, 0)
+    print('Running nearest neighbor with all %d features, ' %(len(empty_set)), 
+          'using \"leaving-one-out\" evaluation, I get accuracy of %.2f' %(accuracy*100)
+          , '%\n')
+
 
 def forward_selection(data):
     print('Beginning Search:')
@@ -23,7 +31,8 @@ def forward_selection(data):
     number_of_features = data.shape[1]
     max_accuracy = 0
     best_set_of_features = set()
-    accuracy_dict = {}
+    start_time = time.time()
+    
     for i in range(1, number_of_features):
         print('On the ' + str(i) + 'th level of the search tree')
         feature_to_add_at_this_level = 0
@@ -63,6 +72,9 @@ def forward_selection(data):
     print('Best set of features: ', best_set_of_features, 
           ' with accuracy: %.2f' %(max_accuracy*100))
           
+    stop_time = time.time() - start_time
+    print('Total time searching: %d' %stop_time, ' seconds')
+    
     return (best_set_of_features, max_accuracy)
 
 def backward_elimination(data):
@@ -73,7 +85,7 @@ def backward_elimination(data):
     # at the number of features
     number_of_features = data.shape[1]    
     max_accuracy = 0
-    
+    start_time = time.time()
     # Start out with all features
     current_set_of_features = set(list(range(1,number_of_features)))
 
@@ -116,8 +128,72 @@ def backward_elimination(data):
                           
     print('Best set of features: ', best_set_of_features, 
           ' with accuracy: %.2f' %(max_accuracy*100))
-          
+
+    stop_time = time.time() - start_time
+    print('Total time searching: %d' %stop_time, ' seconds')
+    
     return (best_set_of_features, max_accuracy)
     
 def marlo_algorithm(data):
-    print('Marlo\'s Special Algorithm stub')
+    # To speed up search we use a modified leave_one_out function. 
+    # The function breaks out of the loop if a threshhold is reached.
+    # This function uses backward elimination search
+    print('Beginning Search:')
+    
+    # Includes the class, but since python for loops
+    # are exclusive of the end value the loop terminates
+    # at the number of features
+    number_of_features = data.shape[1]    
+    max_accuracy = 0
+    percent = 0.20
+    threshhold = int(data.shape[0]*percent)
+    start_time = time.time()
+    
+    # Start out with all features
+    current_set_of_features = set(list(range(1,number_of_features)))
+
+    best_set_of_features = set(list(range(1,number_of_features)))
+    
+    for i in range(1, number_of_features):
+        print('On the ' + str(i) + 'th level of the search tree')
+        feature_to_remove_at_this_level = 0
+        best_so_far_accuracy = 0
+        
+        for k in range(1, number_of_features):
+            level_set = set(list(current_set_of_features))
+            
+            if k in level_set:                
+                level_set.discard(k)
+                
+                accuracy = leave_one_out_modified(data, level_set, 0, threshhold)
+
+                print('\tUsing features ', level_set,
+                      ' accuracy is %.2f' %(accuracy*100), '%')
+                if accuracy > best_so_far_accuracy:
+                    best_so_far_accuracy = accuracy
+                    feature_to_remove_at_this_level = k
+                    
+                if( max_accuracy < accuracy):
+                    max_accuracy = accuracy
+                    best_set_of_features = set(list(level_set))
+        
+        current_set_of_features.discard(feature_to_remove_at_this_level)
+        
+        print('\n\tFeature set ', current_set_of_features, 
+                  ' was best, accuracy is %.2f' %(best_so_far_accuracy*100),
+                  '%\n')        
+        
+        if(max_accuracy > best_so_far_accuracy):
+            print('Warning, accuracy has decreased! Continuing ' +
+                  'seach in case of local maxima')
+            print('Feature set ', best_set_of_features, 
+                  ' was best, accuracy is %.2f' %(max_accuracy*100),
+                  '%\n')
+                          
+    print('Best set of features: ', best_set_of_features, 
+          ' with accuracy: %.2f' %(max_accuracy*100))
+
+    stop_time = time.time() - start_time
+    print('Total time searching: %d' %stop_time, ' seconds')
+    
+    return (best_set_of_features, max_accuracy)
